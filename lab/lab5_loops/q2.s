03f76e0e518db18352f037b08c0d2cb88bc671b9
koa2019 <50386158+koa2019@users.noreply.github.com>
2024-10-16 13:11:02 -0700

    finished and cleaned up lab 5 question 2

--- lab/lab5_loops/q2.s
+++ lab/lab5_loops/q2.s
@@ -7,17 +7,19 @@
 .section .rodata            @ readonly data   
 deref:   .asciz "%d"        @ tells it a number is coming
 derefN:  .asciz "%d\n"
-out1:    .asciz "\n\nInput as many integers as you want and\nthe program will find the largeset and smallest number.\n"
-outGetN: .asciz "\nEnter a positive number. Enter -99 to STOP: " 
+out1:    .asciz "\n\nInput as many integers as you want and\nthe program will find the largeset and smallest number.\n\n"
+outGetN: .asciz "Enter a positive number. Enter -99 to STOP: " 
 outN:    .asciz "\nYou entered: %d\n"
-outMax:    .asciz "\nMax: %d\n"
+outMax:  .asciz "\nMax: %d\n"
 outMin:  .asciz "Min: %d\n"
+outNewMax: .asciz "\t\tNew Max: %d!\n"
+outNewMin: .asciz "\t\tNew Min: %d!\n"
 ty: .asciz "Good Bye\n\n"
 
 
 .align 4
 .section .data
-num: .word 0            @ int num=0
+input: .word 0            @ int input=0
 max: .word 0            @ int max=0
 min: .word 0            @ int min=0
 
@@ -40,65 +42,54 @@ doWhile:
     ldr r0, =outGetN
     bl printf
 
-    @ Get and set variable num with input
+    @ Get and set variable with input
     ldr r0, =deref
-    ldr r1, =num
+    ldr r1, =input
     bl scanf
 
-    @ load num
-    ldr r7, =num
-    ldr r7, [r7]        @ num=r7
+    @ load input
+    ldr r7, =input
+    ldr r7, [r7]        @ input=r7
     
-    @ check if stop flag is true
-    cmp r4, r7          @ (stop-num==?). (r7-r4==?) (99-99==0)  
-    beq endDoWhile      @ if(stop-num==0), then Z==1
+isStop:   
+    cmp r4, r7          @ (stop-input==?). (r7-r4==Z==1). (-99-(-99)==0)  
+    beq endDoWhile      @ if(stop-input==0)->(Z==1)
 
-    @ cmp 
-    @ bgt checkMax        @ while (num<-99) AND (num >-99)
-    @ blt checkMin        @ while (num<-99) AND (num >-99)
-
-    @------- Check for max -------@     
-
-    cmp r7,r5           @ (r7-r5==?). (num-max==?)
-    bgt setMax          @ if(num>=max) N==V
-    ble checkMin
+isMax:
+    cmp r7,r5           @ (r7-r5==?). (input-max==N? V?)
+    bgt setMax          @ if(input>=max)->(N==V)
+    ble isMin        @ if(num<=max)->(Z==1 OR N!=V)
 
 setMax:
-    mov r5, r7          @ max=r7 current num   
-    ldr r0, =outMax
+    mov r5, r7          @ max=r7 reset max with current input   
+    ldr r0, =outNewMax  @ output msg each time a new max is found
     mov r1, r5
     bl printf
-    bal checkMin         @ keep looping while(num!=-99),then Z==0 
+    bal isMin         @ branch to if input is a new min 
 
-    @------- Check for min -------@  
-
-checkMin:  
-    cmp r7, r6          @ (r7-r6==?). (num-min==?)
-    blt setMin          @ if(num<=min). (Z==1 OR N!=V)
-    bge endMin
+isMin:  
+    cmp r7, r6          @ (r7-r6==?). (input-min==N? V?)
+    blt setMin          @ if(input<min)->(N!=V)
+    bge doWhile         @ if(input>=min)->(N==V). branch to doWhile loop
 
 setMin:
-    mov r6, r7
-    ldr r0, =outMin
+    mov r6, r7          @ reset min with current input
+    ldr r0, =outNewMin  @ output msg each time a new max is found
     mov r1, r6
     bl printf
-    bal endMin
+    bal isMin        @ branch to if input is a new min 
 
-endMin:
-    bal doWhile
-
-endDoWhile:
-
+endDoWhile: 
     ldr r0, =outMax
     mov r1, r5
-    bl printf
+    bl printf           @ output max
 
     ldr r0, =outMin
     mov r1, r6
-    bl printf
+    bl printf           @ output min
 
     ldr r0, =ty
-    bl printf
+    bl printf           @ output endOfFile
 
     mov r0, #0  
-    pop {pc}    @return to where i was. Pop whatever on top of stack into program counter r15
+    pop {pc}            @ return 0 to where i was. Pop whatever on top of stack into program counter r15
