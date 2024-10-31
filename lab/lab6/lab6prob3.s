1f57d7a685ddbab237d8a095acc0392a3aeb21a1
koa2019 <50386158+koa2019@users.noreply.github.com>
2024-10-30 19:44:49 -0700

    created lab6prob4.c and it converts fahrenheit to celisus for 0-20F. I used a for loop. lab6prob3.s loops and prints 0-5F to celisus in a for loop

---
+++ lab/lab6/lab6prob3.s
@@ -0,0 +1,34 @@
+@ To compile & run in terminal: gcc -o lab6prob3 lab6prob3.s && ./lab6prob3
+.global main
+
+.align 4 
+.section .rodata @ all constant initalized data goes here
+deref: .asciz "%d"      @ tells it a number is coming
+derefN:  .asciz "%d\n"
+out: .asciz "%dF  == %dC\n"
+
+.align 4
+.text
+main:                   @ int main(){
+    push {lr}           @ push link register r14 to top of stack
+
+    mov r4, #0          @ int i=0
+    mov r5, #5          @ stop loop
+    mov r6, #0          @ Celisus
+
+    for:
+        cmp r4, r5      @ i-20==?
+        bgt endFor      @ if(i>20), then end loop
+
+
+        ldr r0, =out
+        mov r1, r4
+        mov r2, r6
+        bl printf
+
+        add r4, #1
+        bal for
+
+endFor:
+    mov r0, #0  @return 0
+    pop {pc}    @return to where i was. Pop whatever on top of stack into program counter r15
