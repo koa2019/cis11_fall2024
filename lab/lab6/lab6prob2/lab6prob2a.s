675f869a3a863208a19ebbd0e7cafd0e6b16721d
koa2019 <50386158+koa2019@users.noreply.github.com>
2024-10-31 18:45:52 -0700

    lab6prob2a.s calculated distance

--- lab/lab6/lab6prob2/lab6prob2a.s
+++ lab/lab6/lab6prob2/lab6prob2a.s
@@ -1,5 +1,6 @@
-@ compile & run in terminal: gcc lab6prob1a.s && ./a.out
+@ compile & run in terminal: gcc lab6prob2a.s && ./a.out
 .global main
+.global fallingDistance
 
 .align 4
 .section .rodata           @ readonly data   
@@ -7,11 +8,45 @@ deref: .asciz "%d"         @ tells it a number is coming
 derefN: .asciz "%d\n"      @ tells it a number is coming
 outD:    .asciz "\tDistance: %d\n"
 outT:    .asciz "\Time: %d\n"
-outResults: .asciz "%d sec = %d meters\n" @ like scanf syntax in C lang
+out: .asciz "%d sec  = %d meters\n" @ like scanf syntax in C lang
+
+.text
+fallingDistance:        @ int fallingDistance(int time) 
+push {lr}
+pop {pc}
+
 
 .text
 main:                   @ int main(){
     push {lr}           @ push link register r14 to top of stack
 
+    @ set for loop variables
+    mov r4, #1          @ int i=0
+    mov r5, #10         @ stop loop
+    mov r6, #0          @ distance
+
+for:
+        cmp r4, r5      @ i-20==?
+        bgt endFor      @ if(i>20), then end loop
+         
+        @ Calculate distance
+        mov r0, r4
+        mov r1, #9          @ r1 = gravity 9 meters/sec
+        mul r0, r0, r0      @ time = time^2
+        mul r0, r0, r1      @ distance = gravity*time^2
+        mov r0, r0, lsr #1  @ distance/2^1
+        @ bl fallingDistance
+        mov r6, r0
+        
+        @ Output results
+        ldr r0, =out
+        mov r1, r4
+        mov r2, r6              @ mov r2, r9
+        bl printf
+
+        add r4, #1          @ i++
+        bal for             @ branch always for loop
+
+endFor:
     mov r0, #0
     pop {pc}
