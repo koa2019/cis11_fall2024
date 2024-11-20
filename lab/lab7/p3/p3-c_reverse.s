dd8927780dae9ad6e2819e93e1b9239e319d967a
koa2019 <50386158+koa2019@users.noreply.github.com>
2024-11-20 15:04:36 -0800

    lab7 p3-d_bubbleSort.s and p3-e_sort. sort removes Risher's compare and just swaps neighboring indices. reversed() doesn't work properly. I can't figure out how to access revArr[5] instead of revArr[0].

--- lab/lab7/p3/p3-c_reverse.s
+++ lab/lab7/p3/p3-c_reverse.s
@@ -1,9 +1,6 @@
 @ 11-15-24 Lab 7 Array Question 3: Reverse array's order.
 @ compile & run in terminal: 
-@       g++ p3-c.s
-@       ./a.out
-@
-@ printArr outputs a predefined array
+@       g++ p3-c_reverse.s && ./a.out
 .global main
 
 
@@ -17,8 +14,10 @@ outNum:  .asciz "\nUser Array:\n"
 outArr:  .asciz "%d, "
 outUsrArrIndx: .asciz "userArr[%d]=%d == " 
 outRevArrIndx: .asciz "revArr[%d] = %d\n"
-outR: .asciz "\nReversed Array: \n"
-outAfter: .asciz "\n\nAfter...\n\n"
+outRev: .asciz "\nReversed Array: \n"
+outBefore: .asciz "\n\nBefore...\n"
+outInside: .asciz "\n\nInside reversed function...\n\n"
+outAfter: .asciz "\n\nAfter...\n"
 ty: .asciz "\n\nGood Bye\n\n"
 
 
@@ -35,52 +34,53 @@ defRevArr: .word 100, 90, 80, 70, 66, 50
 main:                   @ int main(){
 	str lr, [sp, #-4]!  @ push {lr}
 
-    @ load predefined arr for print function
+    @ load predefined arr for print function    
+    ldr r0, =outBefore
+    bl printf
+
     ldr r0, =outNum
     bl printf
 
-    @ load predefined arr for print function
-    ldr r5, =defUserArr
-    ldr r6, =size
-    ldr r6, [r6]
-    bl printArr         @ printArr(defUserArr[]=r5, size=r6)
+    ldr r0, =defUserArr
+    ldr r1, =size
+    ldr r1, [r1]
+    bl printArr1         @ printArr(defUserArr[]=r5, size=r6)
 
-    @ Output prefined revArr[]    
-    ldr r0, =outR
+    @ Load and output prefined revArr[] before anything is altered 
+    ldr r0, =outRev
     bl printf
 
-    @ load predefined revArr for print function
     ldr r0, =defRevArr
     ldr r1, =size
     ldr r1, [r1]
     bl printArr1       @ printArr1(defRevArr[]=r0, size=r1)
 
-    @ Output prefined revArr[]    
+    @ Reverse user array
+    ldr r0, =defUserArr
+    ldr r1, =size
+    ldr r1, [r1]
+    bl reversed         @ reversed(arr=r0,size=r1)
+
+    @ AFTER
+    @ Load and output revArr for print function         
     ldr r0, =outAfter
     bl printf
 
-    @ mov r0, r5
-    @ mov r1, r6
-    @ ldr r1, [r1]
-    @ bl printArr2       @ printArr2(arr[]=r0, size=r1)
+    ldr r0, =outNum
+    bl printf
 
+    ldr r0, =defUserArr
+    ldr r1, =size
+    ldr r1, [r1]
+    bl printArr1         @ printArr(defUserArr[]=r5, size=r6)
 
+    ldr r0, =outRev
+    bl printf
 
-
-    @ ldr r0, =defRevArr
-    @ ldr r1, =size
-    @ ldr r2, [r2]
-    @ bl printArr2       @ printArr2(arr[]=r0, size=r1)
-
-
-    @ @ Reverse array's order
-    @ ldr r0, =outR
-    @ bl printf
-
-    @ ldr r0, =defUserArr
-    @ ldr r1, =size
-    @ ldr r1, [r1]
-    @ bl reversed         @ reversed(arr=r0,size=r1)
+    ldr r0, =defRevArr
+    ldr r1, =size
+    ldr r1, [r1]
+    bl printArr1       @ printArr1(defRevArr[]=r0, size=r1)
 
 end:
     ldr r0, =ty
@@ -96,8 +96,7 @@ reversed:               @ reversed(userArr=r0, size=r1)
 
     push {r4-r8, lr}    @ protect safe registers from being overwritten in function
 
-    @ Do this:
-    @ Reverse array
+    @ Do this: Reverse array
     @ for(int i=0, j=size-1; i<size; i++, j--){
     @     revArr[j]=userArr[i]; }
 
@@ -111,28 +110,36 @@ reversed:               @ reversed(userArr=r0, size=r1)
     @ldr r8, =revArr     @ r8=revArr
     @ldr r8, [r8]        @ load value of revArr[]
 
+
+    @ Output prefined revArr[]    
+    ldr r0, =outInside
+    bl printf
+
     @ {
     forI:                    @ for(int i=0, j=size-1; i<size; i++, j--)
         cmp r4, r6           @ (i-size)==set flags
         bge endForI          @ if(i>=size)(Z==0 or N==V), then exit for loop  
 
-        @ set revArr's last index with userArr's first index value
-        @mov r2, r8
-        @ldr r2, r8
-        @ldr r2, [r2]
-        @str r7, [r8]        @ str src, dest. revArr[j]=userArr[i];
+        @ Reset revArr[j] to equal userArr[i]
+        @ mov r0, r7          @ r0=r7=userArr
+        @ ldr r0, [r0]        @ load userArr[i]
+        @ str r7, [r8]        @ str src, dest --> str userArr[i], revArr[j]
+
+        @ldr r8, [r8]
+        @ldr r7, [r7]
+        @str r7, [r8]        @ str src, dest --> str userArr[i], revArr[j]
 
 
         @ output userArr[i]
-        ldr r0, =outUsrArrIndx      @ =outArr
-        mov r1, r4                  @ i
-        ldr r2, [r7]                @ load value of userArr[i]
+        ldr r0, =outUsrArrIndx      @ 
+        mov r1, r4                  @ r4=i
+        ldr r2, [r7]                @ load value of r7=userArr[i]
         bl printf                   
 
         @ output revArr index
         ldr r0, =outRevArrIndx
-        mov r1, r5
-        ldr r2, [r8]
+        mov r1, r5                  @ r5=j
+        ldr r2, [r8]                @ r2=value of r8=defRevArr[?]
         bl printf
 
 
@@ -176,57 +183,4 @@ printArr1:                      @ printArr1(usedefRevArrrArr[]=r5, size=r6)
 
     endPrintFor1:
         pop {r4-r6, pc}     @ pop {pc}
-        @ {
-
-
-
-printArr:                       @ printArr(userArr[]=r5, size=r6)
-    push {r4-r6, lr}            @ push {lr}
-    mov r4, #0                  @ int i=0
-
     @ {
-    printFor:                   @ output arr
-        cmp r4, r6              @ (i-size)==set flags
-        bge endPrintFor         @ if(i>size)(Z==0 or N==V), then exit for loop  
-
-        @ output arr[i]
-        ldr r0, =outArr
-        ldr r1, [r5]            @ r5=userArr[i]
-        bl printf
-
-        @ incre for loop & the array
-        add r5, #4			@ arr[i]++. +4-bytes
-        add r4, r4, #1      @ i++ 
-        bal printFor            @ keep looping
-
-    endPrintFor:
-        pop {r4-r6, pc}     @ pop {pc}
-        @ {
-
-
-
-@ printArr2:               @ printArr(arr[]=r0, size=r1)
-@     push {r4-r7, lr}    @ push {lr}
-@     mov r4, #0          @ r4=int i=0
-@                         @ r5=arr address counter
-@     mov r6, r0          @ r6=arr address
-@     mov r7, r1          @ r7=size 
-
-@     @ {
-@     printFor2:                   @ output arr
-@         cmp r4, r1              @ (i-size)==set flags
-@         bge endPrintFor2         @ if(i>size)(Z==0 or N==V), then exit for loop  
-
-@         @ output arr[i]
-@         ldr r0, =outArr
-@         ldr r1, [r6]            @ r6=value of arr[]
-@         bl printf
-
-@         @ incre for loop & the array
-@         add r5, #4			@ arr[i+4bytes]
-@         add r4, r4, #1      @ i++ 
-@         bal printFor2        @ keep looping
-
-@     endPrintFor2:
-@         pop {r4-r7, pc}     @ pop {pc}
-@         @ {
