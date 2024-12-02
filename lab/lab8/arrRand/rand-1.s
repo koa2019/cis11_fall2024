a5954a1ceb59959612f6bd85d3dcbd57e3e6f0ee
koa2019 <50386158+koa2019@users.noreply.github.com>
2024-12-02 15:55:13 -0800

    editing lab8 and added Risher's code from week14 part3

--- lab/lab8/arrRand/rand-1.s
+++ lab/lab8/arrRand/rand-1.s
@@ -6,7 +6,6 @@ derefRand: .asciz "\n\tRandom Num: %d \n"
 outStr: .asciz "a[%d] = %d\n"
 
 .section .data
-
 array: .skip 100  		@ int a[25]. array of 25 4-byte elements
 						@ so 25*4 makes 100 byte total space needed
 
@@ -16,13 +15,14 @@ main: 					@ int main(){
 
 	@ get the parmeters ready for printArr
 	@ r0, array
-	@ r1, size
+	@ r1, size=25
 	ldr r0, =array
 	mov r1, #25
 	bl printArr 		@ printArr( a, 25); 
 
 
 	ldr r4, =array		@ load array before i loop
+	mov r1, #25			@ r1=size=25
 	mov r2, #0 			@ r2 = i = 0
 forLoop:				@ for( int i = 0; i < 25; i++ ){
 	
@@ -34,53 +34,61 @@ forLoop:				@ for( int i = 0; i < 25; i++ ){
 
 	@ bl getRandNum
 
-	@ mov r0, #0	   @ r0=0
-	@ bl time  		   @ gets current time time(0)
-	@ bl srand  	   @ sets the seed for the puesdo random number generator
-	@ bl rand  		   @ calls the rand function
+	@ mov r0, #0	     @ r0=0
+	@ bl time  	     @ gets current time time(0)
+	@ bl srand  	     @ sets the seed for the puesdo random number generator
+	@ bl rand  	     @ calls the rand function
 	@ and r0, r0, #99  @ filters out the numbers to reasonable size without it you can get a 32bit number
-	
-	@ mov r9, r0	   @ r9=random number
-	mov r9, #66
+	@ mov r9, r0	     @ r9=random number
+
 
 	@ @ Print random Number
 	@ ldr r0, =derefRand
 	@ mov r1, r9
-	@ bl printf
+	@ b printf
 
+	mov r9, r2			@ r9=r2=i
+	@ mov r9, #66
 
-	@ get next index of array??
-	add r4, r4, r2, lsl #2 @ r4 = r4 + (i*2^2)
+	@ GET ADDRESS OF NEXT INDEX??
+	add r4, r4, r2, lsl #2 	@ r4 = r4 + (i*2^2)
 
 	@ store a value into the array address
 	@ str source, [destination]
-	str r9, [r4]		@ a[i] = i;
-
+	str r9, [r4]		@ arr[i] = i;
 	
+	@ GET ADDRESS OF NEXT INDEX??
+	@ add r4, r4, r2, lsl #2 	@ r4 = r4 + (i*2^2)
+
 	@ Increment i loop
 	add r2, #1			@  i++
 	bal forLoop
 
 forLoopEnd:   			@ }
-	ldr r0, =array
+
+	ldr r0, =array		@ print arr
 	mov r1, #25
-	bl printArr 		@ printArr( a, 25);
+	bl printArr 		@ printArr( arr, 25);
 
 	mov r0, #0
 	pop {pc} 			@ return 0;	
 @ }
 
 
+@@ --------------------------------------------------- @@
+@@ -------------------- FUNCTIONS -------------------- @@
+@@ --------------------------------------------------- @@
+
+
 printArr: 				@ void printArr( int arr[], int size ){
-/*
-r0 = array address
-r1 = size
- */
+@ r0 = array address
+@ r1 = size
+
 	push {r4,lr}
 	mov r4, #0			@ i = 0
-outputLoop:				@ for( int i = 0; i < size; i++ ){
+forLoop2:				@ for( int i = 0; i < size; i++ ){
 	cmp r4, r1			@ i < size
-	bge outputLoopEnd
+	bge forLoop2End
 	
 	push {r0-r3}
 	ldr r2, [r0]		@ loads arr[i]
@@ -91,23 +99,25 @@ outputLoop:				@ for( int i = 0; i < size; i++ ){
 	@ TODO come back
 	add r4, #1			@ i++
 	add r0, r4, lsl #2	@ next address in the array r0=r0+(r4*4)
-	bal outputLoop
-outputLoopEnd:			@ }
+	bal forLoop2
+forLoop2End:			@ }
 	pop {r4,pc} @ return;
 @ }
 
-getRandNum:
-	push {lr}
-	mov r0, #0
-	bl time  @ gets current time time(0)
-	bl srand  @ sets the seed for the puesdo random number generator
-	bl rand  @ calls the rand function
-	and r0, r0, #99  @ filters out the numbers to reasonable size without it you can get a 32bit number
 
-	@ @ Print random Number
-	@ mov r1, r0
-	@ ldr r0, =derefRand
-	@ bl printf
+@ getRandNum:
+@ 	push {lr}
 
-	pop {pc}
+@ 	mov r0, #0
+@ 	bl time  @ gets current time time(0)
+@ 	bl srand  @ sets the seed for the puesdo random number generator
+@ 	bl rand  @ calls the rand function
+@ 	and r0, r0, #99  @ filters out the numbers to reasonable size without it you can get a 32bit number
+
+@ 	@ @ Print random Number
+@ 	@ mov r1, r0
+@ 	@ ldr r0, =derefRand
+@ 	@ bl printf
+
+@ 	pop {pc}
 
