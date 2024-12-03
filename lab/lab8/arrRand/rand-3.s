aae1d28f4752d7c27c7342e3aeb581c1c869040c
koa2019 <50386158+koa2019@users.noreply.github.com>
2024-12-02 20:29:29 -0800

    cleaned up rand-3.s, so I can use it in lab8 problem 2: selectionSort.s

--- lab/lab8/arrRand/rand-3.s
+++ lab/lab8/arrRand/rand-3.s
@@ -4,8 +4,11 @@
 .align 4
 .section .rodata
 deref: .asciz "%d "
-derefRand: .asciz "\n\tRandom Num: %d \n"
+endl: .asciz "\n "
 outStr: .asciz "a[%d] = %d\n"
+outStr2: .asciz "%d, "
+outArr: .asciz "\n\nArray:\n"
+outRandArr: .asciz "\nRandom Array:\n"
 
 .align 4
 .section .data
@@ -17,12 +20,15 @@ main: 					@ int main(){
 	push {lr}
 
 	@@-------- SET RANDOM NUMBER SEED --------@@
-	mov r0, #0	     @ r0=0
-	bl time  	     @ gets current time time(0)
-	bl srand  	     @ sets the seed for the puesdo random number generator
-	
-	
 
+	mov r0, #0	    	@ r0=0
+	bl time  	     	@ gets current time time(0)
+	bl srand  	     	@ sets the seed for the puesdo random number generator
+	
+	@@-------- Print Array --------@@
+
+	ldr r0, =outArr
+	bl printf 		
 	ldr r0, =array
 	mov r1, #25
 	bl printArr 		@ printArr( arr, 25); 
@@ -39,33 +45,30 @@ forLoop:				@ for(i < 25)
 	bge forLoopEnd		@ if(i >= 25) then end loop
 
 
-	@@-------- GET RANDOM NUMBER SEED --------@@
+	@@-------- Call Random Number Function --------@@
 
-	 bl getRandNum	 @ returns r0=randNum
-	 mov r9, r0	     @ r9=random number
-
-	@ @ Print random Number
-	@ ldr r0, =derefRand
-	@ mov r1, r9
-	@ b printf
-
-	@mov r9, r6			@ r9=r6=i. Sets array with 0-24 values
+	bl getRandNum	 	@ returns r0=randNum
+	mov r9, r0	     	@ r9=random number
 
 	@ store a value into the array address	
 	str r9, [r4]		@ arr[i] = i; @ str source, [destination]
 	
-	@ GET ADDRESS OF NEXT INDEX??
-	add r4, r4, r6, lsl #2 	@ r4 = arr + (i*2^2)
-
+	
 	@ Increment i loop
-	add r6, #1			@  i++
-	bal forLoop
+	add r4, r4, r6, lsl #2 @ GET ADDRESS OF NEXT INDEX?? @ r4 = arr + (i*2^2)
+	add r6, #1			   @  i++
+	bal forLoop			   @ keep looping
 
 forLoopEnd:   			
 
+	@@-------- Print random array --------@@
+	ldr r0, =outRandArr
+	bl printf 		
 	ldr r0, =array		
 	mov r1, #25
 	bl printArr 		@ printArr( arr, 25);
+	ldr r0, =endl		@ load string
+	bl printf 			@ printf("\n")
 
 	mov r0, #0
 	pop {pc} 			@ return 0;	
@@ -85,28 +88,38 @@ getRandNum:
 	pop {pc}
 
 
+@@ -------------------- PRINT Function -------------------- @@
 
 printArr: 				@ void printArr( int arr[], int size ){
 @ r0 = array address
 @ r1 = size
 
-	push {r4-r6, lr}		@ protect these registers from this function changing its value
+	@ Get variables ready before loop starts
+	push {r0-r6, lr}	@ protect these registers from this function changing its value
 	mov r4, r0			@ r4=arr address
 	mov r5, #0			@ i=0
 	mov r6, r1			@ r6=size
-forLoop2:				@ for( i < size ){
+printLoop:				@ for( i < size ){
 	cmp r5, r6			@ (i-size==set flags)
-	bge forLoop2End		@ if(i >= size), then end loop
+	bge printLoopEnd	@ if(i >= size), then end loop
 	
-	ldr r0, =outStr		@ load deref string
-	mov r1, r5			@ r1=r5=i
-	mov r2, r4			@ r2=arr address
-	ldr r2, [r2]		@ loads r2 with value of arr[i]
-	bl printf 			@ printf( "a[%d] = %d\n", i, arr[i] );
+	
+	@ ldr r0, =outStr		@ load deref string
+	@ mov r1, r5			@ r1=r5=i
+	@ mov r2, r4			@ r2=arr address
+	@ ldr r2, [r2]		    @ loads r2 with value of arr[i]
+	@ bl printf 			@ printf( "a[%d] = %d\n", i, arr[i] );
+
+	ldr r0, =outStr2	@ load deref string
+	mov r1, r4			@ load arr address
+	ldr r1, [r1]		@ arr[i]
+	bl printf 			@ printf(" %d,", arr[i])
 
 	add r4, r5, lsl #2	@ next address in the array r4=r4+(r5*4)
 	add r5, #1			@ i++
-	bal forLoop2
-forLoop2End:			@ }
-	pop {r4-r6, pc} 	@ return;
+	bal printLoop
+printLoopEnd:			@ }
+	ldr r0, =endl		@ load string
+	bl printf 			@ printf("\n")
+	pop {r0-r6, pc} 	@ return;
 @ }
